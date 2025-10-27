0c614450959489711bf31c8153846d077f17a940
Linus0006 <llatendr@smail.uni-koeln.de>
2025-10-27 16:19:26 +0100

    firstProgress

--- src/h1/H1_main.java
+++ src/h1/H1_main.java
@@ -1,4 +1,14 @@
 package h1;
 
 public class H1_main {
+    static void main(String[] args) {
+
+        //Variable:
+        int z = 12;
+
+        //testing variable:
+        if(z > 10){
+            z = z-10;
+        }
+    }
 }
--- src/h2/H2_main.java
+++ src/h2/H2_main.java
@@ -1,4 +1,26 @@
 package h2;
 
 public class H2_main {
+    static void main(String[] args) {
+
+        //Variables:
+        int i = 528;
+        int j = 15;
+        int k = 23;
+        int min = -1;
+        int max = 13;
+
+        if (i >= j && i >= k) {
+            max = i;
+            System.out.println(max);
+        }
+        if (j >= k && j >= i) {
+            max = j;
+            System.out.println(max);
+        }
+        if (k >= j && k >= i) {
+            max = k;
+            System.out.println(max);
+        }
+    }
 }
