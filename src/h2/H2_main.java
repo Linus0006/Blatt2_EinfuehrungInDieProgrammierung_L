fc9b01f4e7f8da9678ac136cb9f0e27b5f378726
Linus0006 <llatendr@smail.uni-koeln.de>
2025-10-28 13:48:44 +0100

    Nachfrage

--- src/h2/H2_main.java
+++ src/h2/H2_main.java
@@ -4,11 +4,30 @@ public class H2_main {
     static void main(String[] args) {
 
         //Variables:
-        int i = 528;
+        int i = -1;
         int j = 15;
         int k = 23;
         int min = -1;
         int max = 13;
+
+        //compare
+
+        //minimum:
+
+        if (i <= j && i <= k) {
+            min = i;
+            System.out.println(min);
+        }
+        if (j <= k && j <= i) {
+            min = j;
+            System.out.println(min);
+        }
+        if (k <= j && k <= i) {
+            min = k;
+            System.out.println(min);
+        }
+
+        //maximum:
 
         if (i >= j && i >= k) {
             max = i;
--- src/h3/H3_main.java
+++ src/h3/H3_main.java
@@ -1,4 +1,42 @@
 package h3;
 
 public class H3_main {
+    static void main(String[] args) {
+
+        //Variables:
+
+        int i = 201;
+        int j = 100;
+        int k = -10;
+
+        boolean A1 = i > j;
+        boolean A2 = i > 200;
+        boolean A3 = j > 100;
+
+        //compare based on i > j , i > 200 and j > 100
+
+        if (A1 && A2 && A3){
+            k = 3;
+            System.out.println(k);
+        }
+        else if (A1 && A2) {
+            k =2;
+            System.out.println(k);
+
+        }
+        else if (A1) {
+            k = 1;
+            System.out.println(k);
+
+        }
+        else if (!A1 && !A2 && !A3){
+            k = 4;
+            System.out.println(k);
+        }
+        else {
+            k = -10;
+            System.out.println(k);
+        }
+
+    }
 }
