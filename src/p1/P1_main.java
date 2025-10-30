1e9d8029e80196af06488df187e62fa1e8984576
Linus0006 <llatendr@smail.uni-koeln.de>
2025-10-30 12:27:21 +0100

    Präsenz

---
+++ src/p1/P1_main.java
@@ -0,0 +1,12 @@
+package p1;
+
+import java.util.Scanner;
+
+public class P1_main {
+    static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String zeile = new String();
+        zeile = scanner.nextLine();
+        scanner.close();
+    }
+}
---
+++ src/p2/P2_main.java
@@ -0,0 +1,13 @@
+package p2;
+
+public class P2_main {
+    static void main(String[] args) {
+         int i = 69;
+         boolean b = false;
+
+         if (i>10){
+             b = true;
+
+         }
+    }
+}
